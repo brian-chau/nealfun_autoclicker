7ebeb8bd56144c0ebe59ea5576d22031ce97c475
Brian Chau <brian.chau@outlook.com>
2025-01-11 16:53:34 -0800

    Named constants

--- play.py
+++ play.py
@@ -1,16 +1,19 @@
-
 import random
 import pyautogui as py
 
-positions = { "collect": (500,213)  }
-
-c = (492, 423)
+collect_exp_points = (464,213)
+click_me = (490, 430)
+buy_these_y_axis = 670
+buy_these_x_axis_lower_range = 150
+buy_these_x_axis_upper_range = 446
+buy_these_x_step_size = 10
+console_position = ( 1500, 500 )
 while True:
-    for i in range( 300 ):
+    for i in range( 1000000 ):
         py.PAUSE = 0.001
 
-        py.click(x=c[0],y=c[1])
-        py.click(x=positions['collect'][0],y=positions['collect'][1])
-        py.click(x=random.randrange(150,900,30),y=640)
-    py.click(x=1500,y=500)
+        py.click(x=collect_exp_points[0],y=collect_exp_points[1])
+        py.click(x=click_me[0],y=click_me[1])
+        py.click(x=random.randrange(buy_these_x_axis_lower_range,buy_these_x_axis_upper_range,buy_these_x_step_size),y=buy_these_y_axis)
+    py.click(x=console_position[0],y=console_position[1])
     a = input( '> ' )
