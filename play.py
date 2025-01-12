06bf1541618f871ea5ba3ed69a6f872c86d11315
Brian Chau <brian.chau@outlook.com>
2025-01-11 18:02:43 -0800

    Updated script

--- play.py
+++ play.py
@@ -3,14 +3,15 @@ import pyautogui as py
 
 collect_exp_points = (464,213)
 click_me = (490, 430)
-buy_these_y_axis = 670
+buy_these_y_axis = 635
 buy_these_x_axis_lower_range = 150
-buy_these_x_axis_upper_range = 446
+#buy_these_x_axis_upper_range = 650
+buy_these_x_axis_upper_range = 446 # Use this to avoid clicking the waterfall
 buy_these_x_step_size = 10
 console_position = ( 1500, 500 )
 while True:
-    for i in range( 1000000 ):
-        py.PAUSE = 0.001
+    for i in range( 2000 ):
+        py.PAUSE = 0.01
 
         py.click(x=collect_exp_points[0],y=collect_exp_points[1])
         py.click(x=click_me[0],y=click_me[1])
